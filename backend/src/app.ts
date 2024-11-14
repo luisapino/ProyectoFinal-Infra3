143570a29705ef38adf4fdc693e4b6a25adc4240
Santiago0212004 <sjbs0212@gmail.com>
2024-11-13 19:49:41 -0500

    Permissive cors policy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,11 @@ import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 
 app.use("/auth", authRoutes);
