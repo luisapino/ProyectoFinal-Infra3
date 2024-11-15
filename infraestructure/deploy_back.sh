653e6ba5b729ca243eee1d9b7ac94f0dce25756f
Santiago0212004 <sjbs0212@gmail.com>
2024-11-14 23:23:51 -0500

    Cleanup repository and updated deploy_front and deploy_back scripts

--- infraestructure/deploy_back.sh
+++ infraestructure/deploy_back.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 yum update -y
 yum install -y git
-
 sudo -u ec2-user bash -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
-
 export NVM_DIR="/home/ec2-user/.nvm"
-
 sudo -u ec2-user bash -c 'export NVM_DIR="/home/ec2-user/.nvm" && \
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
-nvm install v16.20.2 && nvm alias default v16.20.2'
-
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
+    nvm install v16.20.2 && nvm alias default v16.20.2'
 echo 'export NVM_DIR="$HOME/.nvm"' >> /home/ec2-user/.bashrc
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> /home/ec2-user/.bashrc
 echo 'export PATH="$NVM_DIR/versions/node/v16.20.2/bin:$PATH"' >> /home/ec2-user/.bashrc
-
 sudo -u ec2-user bash -c 'export NVM_DIR="/home/ec2-user/.nvm" && \
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
     npm install -g yarn pm2'
-
 sudo -u ec2-user git clone https://github.com/luisapino/ProyectoFinal-Infra3.git /home/ec2-user/ProyectoFinal-Infra3
 cd /home/ec2-user/ProyectoFinal-Infra3
 sudo -u ec2-user git checkout back
-
 cd /home/ec2-user/ProyectoFinal-Infra3/backend
-
 sudo -u ec2-user bash -c 'export NVM_DIR="/home/ec2-user/.nvm" && \
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
     yarn install && yarn build'
-
 cat <<EOF > /home/ec2-user/ProyectoFinal-Infra3/backend/.env
 PORT=${BackendPort}
 JWT_SECRET=${JwtSecret}
@@ -37,9 +28,8 @@ DB_USERNAME=${DBUsername}
 DB_PASSWORD=${DBPassword}
 DB_DATABASE=${DBName}
 EOF
-
 sudo -u ec2-user bash -c 'export NVM_DIR="/home/ec2-user/.nvm" && \
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
-    pm2 start yarn -- start
-    pm2 save
+    pm2 start yarn -- start && \
+    pm2 save && \
     pm2 startup'
--- infraestructure/deploy_front.sh
+++ infraestructure/deploy_front.sh
@@ -1,17 +1,83 @@
-yum update
-yum install httpd
-systemctl start httpd
-systemctl enable httpd
-yum install -y git
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install epel -y
+sudo yum install -y stress
+sudo yum install -y nginx
+sudo yum install -y git
+
 cd /home/ec2-user/
 git clone https://github.com/luisapino/ProyectoFinal-Infra3.git
 cd /home/ec2-user/ProyectoFinal-Infra3/
 git checkout front
-cd /home/ec2-user/ProyectoFinal-Infra3/Frontend/
+
+cd /home/ec2-user/ProyectoFinal-Infra3/Frontend/script
+sudo rm API.js
 cat <<EOF > /home/ec2-user/ProyectoFinal-Infra3/Frontend/script/API.js
-export const backendUrl = ${BackendLoadBalancer.DNSName};
-export const backendPort = ${BackendPort};
+export const backendUrl = 'http://${FrontendALB.DNSName}';
+export const backendPort = '80';
 EOF
+
 cd /home/ec2-user/ProyectoFinal-Infra3/Frontend/
-sudo cp /home/ec2-user/ProyectoFinal-Infra3/Frontend/ -r /var/www/html/
-sudo systemctl restart httpd
+sudo cp -r . /usr/share/nginx/html/
+
+sudo tee /etc/nginx/nginx.conf > /dev/null <<EOL
+user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+    sendfile on;
+    keepalive_timeout 65;
+
+    server {
+        listen 80;
+
+        location / {
+            root /usr/share/nginx/html;
+            index index.html index.htm;
+        }
+
+        location /health/ {
+            proxy_pass http://${BackendALB.DNSName}:${BackendPort};
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
+        }
+
+        location /auth/ {
+            proxy_pass http://${BackendALB.DNSName}:${BackendPort};
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
+        }
+
+        location /products/ {
+            proxy_pass http://${BackendALB.DNSName}:${BackendPort};
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
+        }
+
+        location /cart/ {
+            proxy_pass http://${BackendALB.DNSName}:${BackendPort};
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
+        }
+    }
+}
+EOL
+
+sudo chkconfig nginx on
+sudo service nginx start
